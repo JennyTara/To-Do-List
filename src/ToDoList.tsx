b46b8007fda2479dfd253330cc168ff1a5d8d4bb
Yauheniya <Yauheniya_Tarasenka@epam.com>
2024-02-25 02:07:31 +0300

    feat: added validation to the input, added submit for the button by enter

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -8,18 +8,34 @@ import './ToDoList.css'
 export default function ToDoList() {
   const [todos, setTodos] = useState<string[]>([])
   const [inputValue, setInputValue] = useState<string>('')
+  const [inputError, setInputError] = useState(false)
   const [checkedItem, setCheckedItem] = useState<boolean>(false)
   const [isCompleted, setCompleted] = useState<boolean>(false)
+  const [showTextEror, setShowTextError] = useState<string>('')
 
 
   function addTask(): void {
-    if (inputValue.trim() !== '') {
+    if (inputValue.trim() !== '' && inputValue.length <= 150) {
       setCheckedItem(false)
       setTodos([...todos, inputValue])
+    } else {
+      setInputError(true)
+      if (inputValue.trim() == '') {
+        setShowTextError("*You must write something!")
+      } else {
+        setShowTextError("*Your ToDo is too long, please shorten it!")
+      }
     }
     setInputValue('')
   }
+  function handleSubmitInput(event: React.KeyboardEvent<HTMLInputElement>): void {
+    if (event.code === "Enter") {
+      addTask()
+    }
+  }
   function handleChangeInput(event: React.ChangeEvent<HTMLInputElement>): void {
+    setInputError(false)
+    setShowTextError("")
     setInputValue(event.target.value)
   }
   function handleChangeCheckbox(): void {
@@ -27,15 +43,18 @@ export default function ToDoList() {
     setCompleted(!isCompleted)
   }
   function handleDelete(index: number): void {
-    setTodos(todos.filter(item => todos.indexOf(item) !== index))
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
   }
 
   return (
     <>
       <div className="add-item-wrapper">
-        <input type="text" value={inputValue} onChange={handleChangeInput} className="add-item__input" placeholder="Create a new ToDo..." />
+        <input type="text" value={inputValue} onChange={handleChangeInput} onKeyDown={handleSubmitInput} tabIndex={1} className={inputError ? "add-item__input error" : "add-item__input"} placeholder="Create a new ToDo..." />
         <Button title="add" className="button" onClick={addTask} />
       </div>
+      <div className="input-error">{showTextEror}</div>
       <div className="status-wrapper">
         <Button title="All" className="button" disabled={true} />
         <Button title="Active" className="button" disabled={true} />
@@ -43,13 +62,13 @@ export default function ToDoList() {
       </div>
       <div className="tasks">
         {todos.map((todo: string, index: number) => (
-          <label key={`myToDo-${index}`} className="tasks__item">
+          <div key={`myToDo-${index}`} className="tasks__item">
             <div className="tasks__checkbox-text" >
               <input className="tasks__checkbox" type="checkbox" checked={checkedItem} onChange={handleChangeCheckbox} />
               <div className={isCompleted ? "tasks__text completed" : "tasks__text"}>{todo}</div>
             </div>
             <Button title="Delete" className="button" onClick={() => handleDelete(index)} />
-          </label>
+          </div>
         ))}
 
       </div>
