6d36b453f7d5dac90549afcd435acf256a56bcb6
Yauheniya <Yauheniya_Tarasenka@epam.com>
2024-02-29 23:44:17 +0300

    feat: added functionality to status buttons + fixed bug with checkboxes

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -8,6 +8,7 @@ interface TodoList {
   title: string;
   isCompleted: boolean;
 }
+let countId = 0
 // const initialTodos = [
 //   { index: 1, title: "First task", isCompleted: false },
 //   { index: 2, title: "Second task", isCompleted: false },
@@ -17,15 +18,20 @@ interface TodoList {
 export default function ToDoList () {
   const [todos, setTodos] = useState<TodoList[]>([])
   const [inputValue, setInputValue] = useState<string>('')
-  const [inputError, setInputError] = useState(false)
+  const [inputError, setInputError] = useState<boolean>(false)
   const [showTextEror, setShowTextError] = useState<string>('')
-  const [disableStatusButton, setDisableStatusButton] = useState(true)
+  const [disableStatusButton, setDisableStatusButton] = useState<boolean>(true)
+  const [filter, setFilter] = useState<string>('all')
+
 
 
   function addTask(): void {
     if (inputValue.trim() !== '' && inputValue.length <= 150) {
-      setTodos([...todos, { index: (todos.length), title: inputValue, isCompleted: false }])
-      console.log(todos)
+
+      setTodos([...todos, { index: countId++, title: inputValue, isCompleted: false }])
+      if(todos.length==0) {
+        setDisableStatusButton(false)
+      }
     } else {
       setInputError(true)
       if (inputValue.trim() == '') {
@@ -33,8 +39,10 @@ export default function ToDoList () {
       } else {
         setShowTextError("*Your ToDo is too long, please shorten it!")
       }
+
     }
     setInputValue('')
+    console.log(todos)
   }
   function handleSubmitInputByEnter(event: React.KeyboardEvent<HTMLInputElement>): void {
     if (event.code === "Enter") {
@@ -47,16 +55,20 @@ export default function ToDoList () {
     setInputValue(event.target.value)
   }
   function handleChangeCheckbox(id: number): void {
-    const copyTodos = [...todos]
-    copyTodos[id].isCompleted = !copyTodos[id].isCompleted
-    setTodos(copyTodos)
+    setTodos(todos.map((todo) => {
+      if (todo.index === id) {
+        todo.isCompleted = !todo.isCompleted
+      }
+      return todo
+    }))
   }
+
   function handleDelete(id: number): void {
     setTodos(todos.filter(item => item.index !== id))
-  }
-  function handleShowAll (): void {
-    setTodos(todos)
-
+    console.log(todos)
+    if(todos.length ==1) {
+      setDisableStatusButton(true)
+    }
   }
 
   return (
@@ -67,12 +79,22 @@ export default function ToDoList () {
           </div>
           <div className="input-error">{showTextEror}</div>
           <div className="status-wrapper">
-            <Button title="All" className="button" onClick={handleShowAll} disabled={disableStatusButton} />
-            <Button title="Active" className="button" disabled={disableStatusButton} />
-            <Button title="Completed" className="button" disabled={disableStatusButton} />
+            <Button title="All" className={((todos.length) && filter=='all'  ) ? 'button active-filter' : 'button'} onClick={()=> setFilter('all')}  disabled={disableStatusButton}/>
+            <Button title="Active" className={filter=='active'? 'button active-filter' : 'button'} onClick={()=> setFilter('active')} disabled={disableStatusButton} />
+            <Button title="Completed" className={filter=='completed'? 'button active-filter' : 'button'} onClick={()=> setFilter('completed')} disabled={disableStatusButton} />
           </div>
           <div className="tasks">
-            {!todos.length ? <div>There is no task to do...</div> : todos.map((todo) => (
+            {!todos.length ?
+            <div>There is no task to do...</div> :
+            todos.filter(todo => {
+              if (filter === 'all') {
+                return todos;
+              } else if (filter === 'active') {
+                return !todo.isCompleted;
+              } else if (filter === 'completed') {
+                return todo.isCompleted;
+              }})
+              .map((todo) => (
               <div key={`myToDo-${todo.index}`} className="tasks__item">
                 <div className="tasks__checkbox-text" >
                   <input className="tasks__checkbox" type="checkbox" checked={todo.isCompleted} onChange={() => handleChangeCheckbox(todo.index)} />
